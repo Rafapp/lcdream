ef2c45c2e8778d51b3fa2c03b9ff0cae39d8d108
Rafagamedev <rpadiper@gmail.com>
2026-05-03 14:32:30 -0600

    Added precompiled header support

---
+++ lcdream/include/pch.h
@@ -0,0 +1,21 @@
+// STL
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <cstdint>
+
+// OpenGL / windowing (order matters: glad before GLFW)
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+// Math
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// UI
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
